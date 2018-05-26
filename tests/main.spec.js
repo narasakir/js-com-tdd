cdc99cb447aa2ea8a7119ec9d8d908b28c341ceb
Raphael Narasaki <narasakir@gmail.com>
2018-05-26 14:40:14 -0300

    Add check-coverage-to prepush hook

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,55 +1,25 @@
 import { expect } from 'chai'
-import { sum, sub, mult, div } from '../src/main'
+import FizzBuzz from '../src/main'
 
-describe('Calc', () => {
-  // smoke tests
-  describe('Smoke tests', () => {
-
-    it('should exists the method sum', () => {
-      expect(sum).to.exist
-      expect(sum).to.be.a('function')
-    })
-    it('should exists the method sub', () => {
-      expect(sub).to.exist
-      expect(sub).to.be.a('function')
-    })
-    it('should exists the method mult', () => {
-      expect(mult).to.exist
-      expect(mult).to.be.a('function')
-    })
-    it('should exists the method div', function () {
-      expect(div).to.exist
-      expect(div).to.be.a('function')
-    })
+describe('FizzBuzz', () => {
+  it('should return `Fizz` when multiple of 3', () => {
+    expect(FizzBuzz(3)).to.be.equal('Fizz')
+    expect(FizzBuzz(6)).to.be.equal('Fizz')
   })
 
-  describe('Sum', () => {
-    it('should return 4 when `sum(2,2)`', () => {
-      expect(sum(2, 2)).to.be.equal(4)
-    })
-  })
-  describe('Sub', () => {
-    it('should return 4 when `sub(6,2)`', () => {
-      expect(sub(6, 2)).to.be.equal(4)
-    })
-    it('should return -4 when `sub(6,10)`', () => {
-      expect(sub(6, 10)).to.be.equal(-4)
-    })
+  it('should return `Buzz` when multiple of 5', () => {
+    expect(FizzBuzz(5)).to.be.equal('Buzz')
+    expect(FizzBuzz(10)).to.be.equal('Buzz')
   })
 
-  describe('Mult', () => {
-    it('should return 12 when `mult(6,2)`', () => {
-      expect(mult(6, 2)).to.be.equal(12)
-    })
+  it('should return `FizzBuzz` when multiple of 3 and 5', () => {
+    expect(FizzBuzz(15)).to.be.equal('FizzBuzz')
   })
 
-  describe('Div', () => {
-    it('should return 5 when `div(25,5)`', () => {
-      expect(div(25, 5)).to.be.equal(5)
-    })
-    it('should return `Não é possivel divisao por zero` when divide by 0', () => {
-      expect(div(4, 0)).to.be.equal('Não é possivel divisao por zero')
-    })
+  it('should return the number when non-multiple', () => {
+    expect(FizzBuzz(7)).to.be.equal(7)
   })
-
+  it('should return 0 when 0', () => {
+    expect(FizzBuzz(0)).to.be.equal(0)
+  })
 })
